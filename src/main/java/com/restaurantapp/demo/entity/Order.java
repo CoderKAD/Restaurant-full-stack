6ae775517e077fd1c687d32b70d4505fbec362e0
CoderKAD <karrakhadija728@gmail.com>
2026-02-09 17:44:06 +0100

    Initial commit

--- src/main/java/com/restaurantapp/demo/entity/Order.java
+++ src/main/java/com/restaurantapp/demo/entity/Order.java
@@ -5,8 +5,7 @@ import com.restaurantapp.demo.entity.enums.OrderStatus;
 import com.restaurantapp.demo.entity.enums.OrderType;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
